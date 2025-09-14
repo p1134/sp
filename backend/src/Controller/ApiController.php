382ff54703ec38b1d2a847c5a259ba19375c6631
Paulina <paulinarygula5@gmail.com>
2025-09-14 18:36:30 +0200

    Update react-scripts to version 5.0.1 in package.json

--- backend/src/Controller/ApiController.php
+++ backend/src/Controller/ApiController.php
@@ -9,12 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
-    #[Route('/api/data', name: 'firebase_data')]
-    public function index(Database $database): JsonResponse
-    {
-        $reference = $database->getReference('/pomiar');
+#[Route('/api/data', name: 'firebase_data')]
+public function index(Database $database): JsonResponse
+{
+    try {
+        $reference = $database->getReference('users/paulina');
         $data = $reference->getValue();
-
         return $this->json($data);
+    } catch (\Throwable $e) {
+        return $this->json(['error' => $e->getMessage()], 500);
     }
+}
+
 }
